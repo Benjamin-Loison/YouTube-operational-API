90fcedf0bd876c4f29bc2efee7989a4d1049f4b6
Benjamin Loison <12752145+Benjamin-Loison@users.noreply.github.com>
2023-02-02 14:23:44 +0100

    Add comments to `addKey.php`

--- addKey.php
+++ addKey.php
@@ -4,9 +4,11 @@
 
     if (isset($_GET['key'])) {
         $key = $_GET['key'];
+        // Regex-based filter.
         if (isYouTubeDataAPIV3Key($key)) {
             $keysContent = file_get_contents(KEYS_FILE);
             $keys = explode("\n", $keysContent);
+            // Verify that the YouTube Data API v3 key isn't already stored by the instance.
             if (!in_array($key, $keys)) {
                 $httpOptions = [
                     'http' => [
@@ -18,8 +20,10 @@
                     file_put_contents(KEYS_FILE, ($keysContent === '' ? '' : "\n") . $key, FILE_APPEND);
                     echo 'YouTube Data API v3 key added.';
                 } elseif ($content['error']['errors'][0]['reason'] === 'quotaExceeded') {
+                    // As users can set `Queries per minute` quota to 0, we avoid denial-of-service by not considering them.
                     echo 'Not adding YouTube Data API v3 key having quota exceeded.';
                 } else {
+                    // This YouTube Data API API v3 keys isn't assigned or there is another error.
                     echo 'Incorrect YouTube Data API v3 key.';
                 }
             } else {
