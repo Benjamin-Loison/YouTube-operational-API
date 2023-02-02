51059e89caa12cd8401219202514f17b28a0b287
Benjamin Loison <12752145+Benjamin-Loison@users.noreply.github.com>
2023-02-02 14:16:08 +0100

    Extract instance configuration from `constants.php` to `configuration.php`

---
+++ configuration.php
@@ -0,0 +1,7 @@
+<?php
+
+    define('KEYS_FILE', '/var/www/ytPrivate/keys.txt');
+    define('SERVER_NAME', 'my instance');
+    define('GOOGLE_ABUSE_EXEMPTION', '');
+
+?>
--- constants.php
+++ constants.php
@@ -1,17 +1,16 @@
 <?php
 
+    include_once 'configuration.php';
+
     define('DOMAIN_NAME', $_SERVER['SERVER_NAME']);
     $protocol = (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) == 'on')) ? 'https' : 'http';
     define('WEBSITE_URL', "$protocol://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
     define('SUB_VERSION_STR', '.9999099');
-    define('KEYS_FILE', '/var/www/ytPrivate/keys.txt');
-    define('SERVER_NAME', 'my instance');
 
     define('MUSIC_VERSION', '2' . SUB_VERSION_STR);
     define('CLIENT_VERSION', '1' . SUB_VERSION_STR);
     define('UI_KEY', 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8'); // this isn't a YouTube Data API v3 key
     define('USER_AGENT', 'Firefox/100');
-    define('GOOGLE_ABUSE_EXEMPTION', '');
     define('HTTP_CODE_DETECTED_AS_SENDING_UNUSUAL_TRAFFIC', 302);
 
 ?>
