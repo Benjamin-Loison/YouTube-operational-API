9db4c71f7008abba9a61e80ca4b9b4e603881f2a
Benjamin Loison <12752145+Benjamin-Loison@users.noreply.github.com>
2023-05-31 11:57:48 +0200

    Clean `tools/checkOperationnalAPI.py`

--- tools/checkOperationnalAPI.py
+++ tools/checkOperationnalAPI.py
@@ -1,29 +1,21 @@
-import json, requests
+#!/usr/bin/python3
 
-channelId = 'UCt5USYpzzMCYhkirVQGHwKQ'
+import requests
+
 query = 'test'
-
-def getURL(url):
-    return requests.get(url).text
 
 videoIds = []
 
 def getVideos(pageToken = '', callsIndex = 0):
     global videoIds
-    url = 'https://yt.lemnoslife.com/'
-    #url += "search?part=snippet&channelId=" + channelId + "&order=viewCount"
-    url += "search?part=id&q=" + query + "&type=video"
+    url = f'https://yt.lemnoslife.com/search?part=id&q={query}&type=video'
     if pageToken != '':
         url += '&pageToken=' + pageToken
-    res = getURL(url)
-    data = json.loads(res)
+    data = requests.get(url).json()
     for item in data['items']:
-        #print(item)
         videoId = item['id']['videoId']
-        #print(videoId)
         if not videoId in videoIds:
             videoIds += [videoId]
-            #print(len(videoIds), videoId)
     print(len(videoIds), callsIndex)
     if 'nextPageToken' in data:
         getVideos(data['nextPageToken'], callsIndex + 1)
