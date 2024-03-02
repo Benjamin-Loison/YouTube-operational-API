c0baf2e81671f8d46aeed1ae478dc7a84d5d784b
Benjamin Loison <12752145+Benjamin-Loison@users.noreply.github.com>
2024-03-02 02:15:03 +0100

    Fix #244: Remove useless `echo` `` for generating `instanceKey`

--- configuration.php
+++ configuration.php
@@ -3,7 +3,7 @@
     define('KEYS_FILE', 'ytPrivate/keys.txt');
     define('SERVER_NAME', 'my instance');
     define('GOOGLE_ABUSE_EXEMPTION', '');
-    // Both following entries can be generated with `tr -dc A-Za-z0-9 </dev/urandom | head -c 32 ; echo ''`.
+    // Both following entries can be generated with `tr -dc A-Za-z0-9 </dev/urandom | head -c 32 ; echo`.
     define('RESTRICT_USAGE_TO_KEY', '');
     // If not defined, a random value will be used to prevent denial-of-service.
     define('ADD_KEY_FORCE_SECRET', '');
